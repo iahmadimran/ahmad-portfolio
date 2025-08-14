faa13899c956b23fcaa7e0782d478fe1442422e2
Ahmad Imran <ahmadimran67208@gmail.com>
2025-08-14 10:43:48 +0500

    setup next js error config

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -4,7 +4,7 @@ import { gridItems } from '@/data'
 
 const Grid = () => {
   return (
-    <section id='about' className='pt-40 pb-28'>
+    <section id='about' className='pt-20 pb-28'>
       <div className='flex justify-center items-center flex-col'>
         <h1 className='heading'>
           About <span className='text-purple'>Me</span>
