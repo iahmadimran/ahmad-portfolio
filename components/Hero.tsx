1cd3216db9f1ca493b96fd69a39ac3c6d3a54063
Ahmad Imran <ahmadimran67208@gmail.com>
2025-08-14 16:55:33 +0500

    updated hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -20,18 +20,6 @@ const Hero = () => {
         <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
       </div>
 
-      {/* <div className="flex h-screen w-full items-center justify-center bg-white dark:bg-black-100 absolute top-0 left-0">
-        <div
-          className={cn(
-            "absolute inset-0",
-            "[background-size:40px_40px]",
-            "[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
-            "dark:[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]",
-          )}
-        />
-        <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black-100" />
-      </div> */}
-
       <div className='flex justify-center items-center relative my-10'>
         <div className='max-w-[89vw] md:max-w-6xl lg:max-w-[60vw] flex flex-col items-cemter justify-center'>
           <h2 className='uppercase tracking-widest text-xs text-blue-100 text-center w-full'>
